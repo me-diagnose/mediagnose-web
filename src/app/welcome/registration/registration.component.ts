764d7d0a0119333748a7a12df4f2833149e9f93b
Hamza Nouinou <nouinouhamza@gmail.com>
2021-12-07 00:08:50 +0100

    feat: add angular material to registration and login forms

--- src/app/welcome/registration/registration.component.ts
+++ src/app/welcome/registration/registration.component.ts
@@ -5,8 +5,8 @@ import {IRegistration} from "../../shared/interfaces/user.interface";
 import {Router} from "@angular/router";
 import {mustMatch} from "../../utils/mustMatch.util";
 
-const USERNAME_MAX_LENGTH = 20;
-const PASSWORD_MIN_LENGTH = 6
+const USERNAME_MAX_LENGTH = 30;
+const PASSWORD_MIN_LENGTH = 6;
 
 @Component({
   selector: 'app-registration',
@@ -25,14 +25,19 @@ export class RegistrationComponent implements OnInit {
     this.buildRegistrationForm();
   }
 
+  isError(controlName: string): boolean {
+    return this.registrationForm.get(controlName)?.touched && !this.registrationForm.get(controlName)?.valid || false;
+  }
+
   buildRegistrationForm(): void {
     this.registrationForm = this.fb.group({
         username: [null, [Validators.maxLength(this.usernameMaxLength), Validators.required]],
         email: [null, [Validators.email, Validators.required]],
         password: [null, [Validators.required, Validators.minLength(this.passwordMinLength)]],
         confirmPassword: [null, Validators.required],
-        sex: [null, [Validators.required]],
-        age: [null, [Validators.min(18), Validators.max(100),Validators.required]],
+        // Todo: Put age and sex controls back when we have a database
+        // sex: [null, [Validators.required]],
+        // age: [null, [Validators.min(18), Validators.max(100),Validators.required]],
         acceptTerms: [false, Validators.requiredTrue]
       },
       {
@@ -49,6 +54,7 @@ export class RegistrationComponent implements OnInit {
   }
 
   onSubmit(): void {
+    console.log(this.registrationForm)
     this.authService.register$(this.registrationForm.value as IRegistration).subscribe((registered: boolean) => {
       if(registered) {
         this.router.navigate(['..','payment'])
---
+++ src/app/welcome/welcome-material.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+@NgModule({
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule],
+  exports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatCheckboxModule]
+})
+export class WelcomeMaterialModule {
+}
