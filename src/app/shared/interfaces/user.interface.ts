09b8c0a3c9c03f38e4e1079d4db2aa71815eeab6
Hamza Nouinou <nouinouhamza@gmail.com>
2021-12-12 17:16:08 +0100

    feat: update interfaces

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -1,10 +1,19 @@
 export interface IUser {
   username: string;
   email: string;
+  age: number;
+  gender: UserGender;
 }
 
 export interface IRegistration {
   username: string;
   email: string;
   password: string;
+  age: number;
+  gender: UserGender;
+}
+
+export enum UserGender {
+  MALE = 'male',
+  FEMALE = 'female'
 }
