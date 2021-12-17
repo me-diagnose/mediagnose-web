45a3c8a4092cdc614e82b57ac6022ac8ecc1024a
Hamza Nouinou <nouinouhamza@gmail.com>
2021-12-17 20:25:37 +0100

    feat: capture payment

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PaymentService} from '../shared/services/payment.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  showThankYou = false;
+
+  constructor(private route: ActivatedRoute, private paymentService: PaymentService) {
+  }
+
+  ngOnInit(): void {
+    const paymentToken = this.route.snapshot.queryParamMap.get('token');
+    if(!!paymentToken) {
+      this.paymentService.approvePayment(paymentToken).subscribe(
+        () => {
+          this.showThankYouMessage();
+        }
+      );
+    }
+  }
+
+  showThankYouMessage(): void {
+    this.showThankYou = true;
+  }
 }
