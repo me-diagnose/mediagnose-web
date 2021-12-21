e52bd3f90abaa3ce73fbd8ef334edcbbad83ce5f
Hamza Nouinou <nouinouhamza@gmail.com>
2021-12-21 17:13:00 +0100

    chore: env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   tokeyKeyName: 'MD_Token',
   orderDateKeyName: 'MD_Order_Date',
   mediagnoseAPI: {
-    salt: '<YOUR SALT>',
-    url: '<YOUR API URL>'
+    salt: '$2a$10$cKN3EjVTamPjnw3m0Hk1De',
+    url: 'http://localhost:3000'
   },
   endlessMedical: {
     checkerUrl: 'https://api-prod.endlessmedical.com/v1/dx/',
