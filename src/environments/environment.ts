913eab00f0d8970dac36750b73dac5fbb17005f4
Hamza Nouinou <nouinouhamza@gmail.com>
2022-02-22 00:05:32 +0100

    fix: vulnerabilities

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: 'false',
+    tokeyKeyName: 'MD_Token',
+    orderDateKeyName: 'MD_Order_Date',
+    mediagnoseAPI: {
+      salt: '$2a$10$cKN3EjVTamPjnw3m0Hk1De',
+      url: 'http://localhost:3000',
+    },
+    endlessMedical: {
+      checkerUrl: 'https://api-prod.endlessmedical.com/v1/dx/',
+      sessionKey: 'EM_Session'
+    }
+};
